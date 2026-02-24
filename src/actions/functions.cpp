0d91bf66d43ae3d616fdc18a4d97c29b3c2a0132
lavoinet <etienne.lavoine@grenoble-inp.org>
2026-02-24 14:55:39 +0100

    adding stock orientation

--- src/actions/functions.cpp
+++ src/actions/functions.cpp
@@ -366,7 +366,9 @@ bool m_isPointInsideRectangle(float px, float py, float cx, float cy, float w, f
 void opponentInAction(position_t position){
     for(int i = 0; i < STOCK_COUNT; i++){
         position_t stock_pos = STOCK_POSITIONS_TABLE[i];
-        if (m_isPointInsideRectangle(position.x, position.y, stock_pos.x, stock_pos.y, OPPONENT_ROBOT_RADIUS * 2 + STOCKS_WIDTH, OPPONENT_ROBOT_RADIUS * 2 + STOCKS_LENGTH))
+        if (tableStatus.avail_stocks[i] && m_isPointInsideRectangle(position.x, position.y, stock_pos.x, stock_pos.y, 
+            OPPONENT_ROBOT_RADIUS * 2 + (stock_pos.a == 90 ? STOCKS_LENGTH : STOCKS_WIDTH), 
+            OPPONENT_ROBOT_RADIUS * 2 + (stock_pos.a == 90 ? STOCKS_WIDTH : STOCKS_LENGTH)) )// we  consider stock orientation
         {
             LOG_INFO("Opponent in action at stock %d at position (%d,%d)", i, position.x, position.y);
             tableStatus.avail_stocks[i] = false;
@@ -375,7 +377,7 @@ void opponentInAction(position_t position){
     }
     for(int i = 0; i < DROPZONE_COUNT; i++){
         position_t dropzone_pos = DROPZONE_POSITIONS_TABLE[i];
-        if (m_isPointInsideRectangle(position.x, position.y, dropzone_pos.x, dropzone_pos.y, OPPONENT_ROBOT_RADIUS * 2 + DROPZONE_WIDTH, OPPONENT_ROBOT_RADIUS * 2 + DROPZONE_LENGTH))
+        if (tableStatus.dropzone_states[i] == TableState::DROPZONE_EMPTY && m_isPointInsideRectangle(position.x, position.y, dropzone_pos.x, dropzone_pos.y, OPPONENT_ROBOT_RADIUS * 2 + DROPZONE_WIDTH, OPPONENT_ROBOT_RADIUS * 2 + DROPZONE_LENGTH))
         {
             LOG_INFO("Opponent at dropzone %d at position (%d,%d)", i, position.x, position.y);
             tableStatus.dropzone_states[i] = (tableStatus.colorTeam == BLUE) ? TableState::DROPZONE_YELLOW : TableState::DROPZONE_BLUE;
