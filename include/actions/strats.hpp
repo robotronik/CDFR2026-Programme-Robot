00eb78733df5f3c00da38dcf57601660cf23874e
lavoinet <etienne.lavoine@grenoble-inp.org>
2026-02-27 14:06:54 +0100

    rebase done compilation ok

--- include/actions/strats.hpp
+++ include/actions/strats.hpp
@@ -3,14 +3,12 @@
 #include "utils/logger.hpp"
 
 inline void check(colorTeam_t color, int strategy){
-inline void check(colorTeam_t color, int strategy){
     // Check if the color and strategy are valid
     if (color == NONE || strategy < 1 || strategy > 4)
         LOG_ERROR("Invalid color (", color, ") or strategy (", strategy, ")");
 }
 
 // Function to handle the strategy
-inline position_t StratStartingPos(){
 inline position_t StratStartingPos(){
     // Returns the starting position of the robot
     colorTeam_t color = tableStatus.colorTeam;
