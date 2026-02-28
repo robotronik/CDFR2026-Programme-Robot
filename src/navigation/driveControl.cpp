eb150d40c7c0c3d44844cabe2176216246fc4f29
Ludovic A <ludovic.aubineau@grenoble-inp.org>
2026-02-28 16:55:07 +0100

    Revert "don't work"
    This reverts commit fed027da264d05e3933a91728886491dc742f6b1.

--- src/navigation/driveControl.cpp
+++ src/navigation/driveControl.cpp
@@ -24,7 +24,7 @@ void DriveControl::reset() {
     velocity = {0.0, 0.0, 0.0};
     acceleration = {0.0, 0.0, 0.0};
     is_enabled = false;
-    is_slow_mode = false;
+    is_slow_mode = true;
     drive_interface::disable();
     drive_interface::set_target(convertPositionToPacked(target));
     drive_interface::set_coordinates(convertPositionToPacked(position));
