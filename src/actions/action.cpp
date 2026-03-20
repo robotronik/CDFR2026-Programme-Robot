e71ad56227d858018ad89c74090719f4e2c7b9ca
Robotronik <robotronik.phelma@gmail.com>
2026-03-20 17:43:14 +0100

    fixing dum ass bug

--- src/actions/action.cpp
+++ src/actions/action.cpp
@@ -212,6 +212,7 @@ ReturnFSM_t ActionFSM::TakeStock(){
                 }else{
                     LOG_WARNING("FSM_GATHER_DETECT: Drop Zone is empty");
                     gatherStockState = FSM_GATHER_NAV;
+                    tableStatus.setStockAsRemoved(stock_num);
                     stock_num = -1;
                     return FSM_RETURN_DONE;
                 }
