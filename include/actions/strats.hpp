3a48fa56396e7e4defc7749c4595401018e6bc3d
steffguimove <salut.bonjou@gmail.com>
2026-03-28 13:51:32 +0100

    removed wall issue with a*

--- include/actions/strats.hpp
+++ include/actions/strats.hpp
@@ -7,17 +7,17 @@ void check(colorTeam_t color, int strategy);
 // Function to handle the strategy
 position_t StratStartingPos();
 
-int chooseNextStock();
+int chooseNextStock(int& closest_stock, int& closest_offset);
 
 int toAStarDistStock(int stockNum, int stockOffset);
 int toAStarDist(position_t a);
 int chooseStockStrategy(int& stockNum, int& stockOffset);
 
 int GetBestDropZone();
-int getBestStockPositionOff(int stockNum, int& bestDist);
 
-position_t getBestDropZonePosition(int dropzoneNum, bool steal = false);
+int getBestDropZonePosition(int dropzoneNum, position_t& bestPoss, bool steal = false);
 int getBestStealZonePosition(int& bestDropZone, position_t& bestPos);
+int getBestStockPositionOff(int& stockNum, int& bestOffset);
 
 position_t calculateClosestArucoPosition(position_t currentPos);
 position_t getBestIsolatedPosition(position_t centerPos, position_t fromPos);
