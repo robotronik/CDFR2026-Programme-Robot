e7e8e72fe538916c5b80d7bcdb76a26444e5c619
steffguimove <salut.bonjou@gmail.com>
2026-03-31 17:30:29 +0200

    testing

--- src/actions/strats.cpp
+++ src/actions/strats.cpp
@@ -52,7 +52,6 @@ int chooseNextStock(int& closest_stock, int& closest_offset){
         LOG_WARNING("No next stock available");
         return 0;
     }else{
-        LOG_INFO("Best stock to take: ", closest_stock);
         return min;
     }
 }
@@ -114,6 +113,7 @@ int chooseStockStrategy(int& stockNum, int& stockOffset){
             if(!dist){
                 return std::numeric_limits<int>::max();
             }
+            LOG_INFO("Best stock to take: ", stockNum);
             return dist;
         }
         i++;
@@ -124,13 +124,15 @@ int chooseStockStrategy(int& stockNum, int& stockOffset){
         int dist = getBestStockPositionOff(stockNum, stockOffset);
         
         if(!dist){
-                return std::numeric_limits<int>::max();
-            }
-            return dist;
+            return std::numeric_limits<int>::max();
+        }
+        LOG_INFO("Best stock to take: ", stockNum);
+        return dist;
     }
 
     int dist = chooseNextStock(stockNum, stockOffset); // Choose the closest stock if the strategy stocks are not available
     if (stockNum != -1){
+        LOG_INFO("Best stock to take: ", stockNum, " at dist: ", dist);
         return dist;
     }
     //LOG_WARNING("No stock available");
