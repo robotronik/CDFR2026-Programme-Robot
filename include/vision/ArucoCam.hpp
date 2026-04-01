f620298ae5339f817bcb8f55ef3cb3f0a7f107b9
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-04-01 22:02:03 +0200

    calib camera

--- include/vision/ArucoCam.hpp
+++ include/vision/ArucoCam.hpp
@@ -6,7 +6,7 @@
 using json = nlohmann::json;
 
 #define OFFSET_CAM_X 129 // Offset of the camera in mm on the x axis
-#define OFFSET_CAM_Y 0 // Offset of the camera in mm on the y axis
+#define OFFSET_CAM_Y 4.5 // Offset of the camera in mm on the y axis
 #define OFFSET_CAM_A 0 // Offset angle of the camera in degrees
 #define OFFSET_CLAW_Y -32 // Offset to align claw with block, diminuer = plus à droite
 #define OFFSET_STOCK 300
