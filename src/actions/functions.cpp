ba9be56fda4da65729a2abc403a3e83586e74979
Ludovic A <ludovic.aubineau@grenoble-inp.org>
2026-05-04 13:37:54 +0200

    improve

--- src/actions/functions.cpp
+++ src/actions/functions.cpp
@@ -203,7 +203,7 @@ bool snapClaws(bool closed, bool small){
     static int prevTarget = -1;
     int target = closed ? 5 : (small ? 30 : 110);
     if (prevTarget != target){
-        arduino.moveServoSpeed(SERVO_CLAW_CLOSE_1, target, 150);
+        arduino.moveServoSpeed(SERVO_CLAW_CLOSE_1, target, 300);
         prevTarget = target;
     }
     int current = 0;
