b74c04aa92801ee25c502a28be6167801ea67d62
Ludovic A <ludovic.aubineau@grenoble-inp.org>
2026-05-13 18:59:09 +0200

    lidar fix

--- src/main.cpp
+++ src/main.cpp
@@ -292,31 +292,19 @@ int StartSequence()
 
 void GetLidar()
 {
-    static long prev_time_ms = 0;
     static position_t pos_opponent = {3000,0,0};
     double IsDataValid = (fabs(drive.velocity.a) <= 45.0) || (position_distance(drive.position, pos_opponent) < 1000);
-
-    if (lidar.getData() && IsDataValid)
-    {
-        double time_s = double(_millis() - prev_time_ms) / 1000.0; 
-        convertAngularToAxial(lidar.data, lidar.count, drive.position, 150);
-        pathfind_fill_lidar();
-        
+    convertAngularToAxial(lidar.data, lidar.count, drive.position, 150);
+    pathfind_fill_lidar();
+    if (lidar.getData() && IsDataValid){
         // Only update opponent position if the robot is not moving too fast to avoid noise
-        if ((fabs(drive.velocity.a) <= 45.0) && position_opponentV2(lidar.data, lidar.count, drive.position, pos_opponent)){
-            // If it's the first reading, initialize the filtered position
-            // Apply the low-pass filter
+        if ((fabs(drive.velocity.a) <= 45.0) && position_opponentV2(lidar.data, lidar.count, drive.position, pos_opponent) &&
+                (currentState == RUN || currentState == MANUAL) && (_millis() - tableStatus.startTime > 1000)){ // Only update opponent position after 1 second from the start to avoid false readings at the beginning
             tableStatus.pos_opponent.x = pos_opponent.x;
             tableStatus.pos_opponent.y = pos_opponent.y;
-
-            if ((currentState == RUN || currentState == MANUAL) && (_millis() - tableStatus.startTime > 1000)){ // Only update opponent position after 1 second from the start to avoid false readings at the beginning
-                tableStatus.pos_opponent.x = pos_opponent.x;
-                tableStatus.pos_opponent.y = pos_opponent.y;
-                opponentInAction(pos_opponent);            
-            }
+            opponentInAction(pos_opponent);            
+            
         }
-
-        prev_time_ms = _millis();
     }
 }
 
