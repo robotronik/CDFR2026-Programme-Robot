b71a2f1eaa1753ebab43c6ddb2bdacd97a14d274
stefguimove <salut.bonjou@gmail.com>
2026-05-14 20:42:07 +0200

    max speed to 1500

--- src/navigation/driveControl.cpp
+++ src/navigation/driveControl.cpp
@@ -83,7 +83,7 @@ bool DriveControl::drive(position_t pos[], int n, bool slow_mode, bool complete_
 
     // Linear motion
     double position_acceleration = (is_slow_mode ? 60.0 : 150.0); // mm/s²
-    double position_top_speed    =  (is_slow_mode ? 300.0 : 1000.0); // mm/s
+    double position_top_speed    =  (is_slow_mode ? 300.0 : 1500.0); // mm/s
     double current_linear_velocity = position_length(velocity); // mm/s
     double position_speed; // mm/s
 
